a91bc2c2e5774848b2d2a1eb244c7da97d1154e7
Stiven Colorado <90488682+StivenColorado@users.noreply.github.com>
2023-06-02 20:45:12 -0500

    adding
    adding

--- frontend/tienda-online/src/App.js
+++ frontend/tienda-online/src/App.js
@@ -1,9 +1,12 @@
+import './styles/app.css'
+import './styles/animations.css'
+
+import { Menu } from './components/menu'
 function App() {
   return (
     <div className="App">
-      
+      <Menu></Menu>
     </div>
   );
 }
-
 export default App;
---
+++ frontend/tienda-online/src/components/menu.jsx
@@ -0,0 +1,14 @@
+import '../styles/nav.css'
+export const Menu = () => {
+    return(
+        <nav>
+            <div className="logo"></div>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Popular</li>
+                    <li>cards</li>
+                </ul>
+        </nav>
+    );
+}
